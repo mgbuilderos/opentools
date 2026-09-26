03512e907474b15564c3ade706704f8dedaf627b
Maulik Gupta <marketing@brillarescience.com>
2026-09-26 09:57:47 +0530

    feat(formats): add geographic parsers

---
+++ lib/formats/geo/index.ts
@@ -0,0 +1,14 @@
+export {
+  GeoFormatError,
+  haversineMetres,
+  parseGeoJson,
+  parseGpx,
+  parseKml,
+  stripTimestamps,
+  trimEnds,
+  writeGeoJson,
+  writeGpx,
+  writeKml,
+} from './geo';
+
+export type { GeoDocument, GeoFormatErrorCode, Track, TrackPoint } from './geo';
